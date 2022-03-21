727d30de0d4c88d7800fa9fa44ea608b0fc89d92
Eduardo Fonseca da Silva <edufon1503@gmail.com>
2022-03-21 17:01:25 -0300

    Update cores.py

--- Exercícios/cores.py
+++ Exercícios/cores.py
@@ -1,11 +1,13 @@
-# O usuário deve inserir seu nome e sua cor primária favorita e então o nome do usuári
+# O usuário deve inserir seu nome e sua cor primária favorita e então o nome do usuário deve aparecer daquela cor.
 
 nome = str(input('Qual o teu nome? '))
 cor = str(input('Qual a tua cor primária favorita? '))
-paleta = {'limpa':'\033[m', 
-          'azul':'\033[34m',
-          'vermelho':'\033[31m',
-          'amarelo':'\033[33m'}
+
+# paleta = {'limpa':'\033[m', 
+#         'azul':'\033[34m',
+#          'vermelho':'\033[31m',
+#          'amarelo':'\033[33m'}
+
 if cor != 'Azul' and cor != 'Vermelho' and cor != 'Amarelo':
     print('Desculpa, não entendi a qual cor tu se referiu...')
 else:
